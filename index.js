ee5ba24ff6c57e639c6df8f9ed3f46f142b306a1
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-11-12 19:47:19 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        
+    ])
+
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
